6781d5eceb51612b10daeafd7cad29da7cf98339
Yan Ryzhkov <passionary003@gmail.com>
2022-11-03 05:38:47 +0200

    add popup and email send message

--- js/main.js
+++ js/main.js
@@ -30,6 +30,10 @@ var detector = false
 
 
 
+
+
+
+
 // 1. Создаём новый XMLHttpRequest-объект
 let xhr = new XMLHttpRequest();
 let DOGEPrice = []
@@ -204,6 +208,42 @@ function generatePriceSwap() {
 console.log('cryptoValue', cryptoValue)
 console.log('cryptoValue.number', cryptoValue.number)
 console.log('cryptoValue.valute', cryptoValue.valute)
+
+
+
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelector('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+
+    openPopupButtons.addEventListener('click', function () { // Для каждой вешаем обработчик событий на клик
+         // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+        if (detector === false) {
+            message.innerHTML = `${inputPrice.value} ${rc}
+            checkboxUSD: ${checkUSD.value}`
+            popupCost.innerHTML = `${inputPrice.value} ${rc}`
+        } else {
+            message.innerHTML = `${inputPriceSwap.value} ${vcSwap}`
+            popupCost.innerHTML = `${inputPriceSwap.value} ${vcSwap}`
+        }
+
+    })
+
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if(e.target === popupBg) { // Если цель клика - фот, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+    }
+});
 
 xhr.send(null);
 
